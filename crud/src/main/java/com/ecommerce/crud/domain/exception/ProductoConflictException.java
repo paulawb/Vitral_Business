3daad1e2278c2e562ddda7330f4e6e14da4bcd01
paula wilchez <paulawilchesbuitrago@gmail.com>
2026-05-29 11:02:16 -0500

    mejoras en el crud

--- crud/src/main/java/com/ecommerce/crud/domain/exception/ProductoConflictException.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.crud.domain.exception;
-
-public class ProductoConflictException extends RuntimeException {
-    public ProductoConflictException(String message) {
-        super(message);
-    }
-}
--- crud/src/main/java/com/ecommerce/crud/domain/exception/ProductoNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.crud.domain.exception;
-
-public class ProductoNotFoundException extends RuntimeException {
-    public ProductoNotFoundException(String message) {
-        super(message);
-    }
-}
